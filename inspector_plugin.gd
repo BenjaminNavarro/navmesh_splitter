a9869c1973ecd3b6e878e673d37af2e87103c567
Benjamin Navarro <navarro.benjamin13@gmail.com>
2022-11-20 23:20:31 +0100

    feat: separate build & bake + sequential baking + bake at
    NavigationServer can take only one bake request at a time so we need to handle the queuing here

--- inspector_plugin.gd
+++ inspector_plugin.gd
@@ -7,9 +7,14 @@ func can_handle(object):
 
 func parse_category(object: Object, name: String):
 	if name == "NavmeshSplitter":
+		var build_btn = Button.new()
+		build_btn.text = "Build"
+		build_btn.connect("button_down", object, "rebuild")
+		add_custom_control(build_btn)
+
 		var bake_btn = Button.new()
 		bake_btn.text = "Bake"
-		bake_btn.connect("button_down", object, "rebake")
+		bake_btn.connect("button_down", object, "rebake_all")
 		add_custom_control(bake_btn)
 
 		var clear_btn = Button.new()
--- navmesh_splitter.gd
+++ navmesh_splitter.gd
@@ -7,7 +7,7 @@ var size := Vector3.ZERO
 var subdivide_width := 1
 var subdivide_height := 1
 var subdivide_depth := 1
-var edge_connection_margin := 1.0
+var edge_connection_margin := 3.0
 var navigation: NavigationMesh setget set_navigation
 
 
@@ -15,16 +15,20 @@ var _navmesh_instances = []
 var _navmesh_aabb: AABB
 var _last_bake_size: Vector3
 var _last_bake_subdiv: Vector3
-var _navmeshes_to_rebake := 0
-var _navesh_bake_begin_time = {}
-var _navesh_bake_duration = {}
+var _baking_thread := Thread.new()
+var _baking_request := Semaphore.new()
+var _baking_thread_mtx := Mutex.new()
+var _exit_baking_thread := false
+var _navmeshes_to_rebake = []
+
+signal _baking_process_finished
 
 
 func get_class(): return "NavmeshSplitter"
 func is_class(name): return name == "NavmeshSplitter" or .is_class(name)
 
 func _ready():
-	# Needed for maps begin linked to regions, otherwise all map's RID will be zero below
+	# Needed for maps being linked to regions, otherwise all map's RID will be zero below
 	NavigationServer.process(0.0)
 
 	for navmesh_instance in get_children():
@@ -32,22 +36,40 @@ func _ready():
 		var map_rid = NavigationServer.region_get_map(region_rid)
 		NavigationServer.map_set_edge_connection_margin(map_rid, edge_connection_margin)
 
+	var child_idx := 0
+	for x in range(subdivide_width):
+		_navmesh_instances.append([])
+		for y in range(subdivide_height):
+			_navmesh_instances[x].append([])
+			for z in range(subdivide_depth):
+				_navmesh_instances[x][y].append([])
+				var navmesh_instance = get_child(child_idx)
+				_navmesh_instances[x][y][z] = navmesh_instance
+				++child_idx
 
-func rebake():
-	if Engine.editor_hint:
-		var warnings = _get_configuration_warning()
-		if warnings != "":
-			printerr("Cannot bake the navigation meshes because of configuration errors: ", warnings)
-			return
+	_baking_thread.start(self, "_rebaking_process")
 
-	var bake_begin_time := OS.get_ticks_msec()
+
+func _exit_tree():
+	_baking_thread_mtx.lock()
+	_exit_baking_thread = true
+	_baking_thread_mtx.unlock()
+	_baking_request.post()
+	_baking_thread.wait_to_finish()
+
+
+func rebuild():
+	var warnings = _get_configuration_warning()
+	if warnings != "":
+		printerr("Cannot bake the navigation meshes because of configuration errors: ", warnings)
+		return
+
+	var rebuild_begin_time := OS.get_ticks_msec()
+
 	clear()
 
 	_navmesh_aabb = AABB(Vector3.ZERO, Vector3(size.x / subdivide_width, size.y / subdivide_height, size.z / subdivide_depth))
-	_navmeshes_to_rebake = subdivide_width * subdivide_height * subdivide_depth
 	_navmesh_instances = []
-	_navesh_bake_begin_time = {}
-	_navesh_bake_duration = {}
 	for x in range(subdivide_width):
 		_navmesh_instances.append([])
 		for y in range(subdivide_height):
@@ -69,15 +91,39 @@ func rebake():
 				navmesh_instance.navmesh.setup_local_to_scene()
 				navmesh_instance.navmesh.filter_baking_aabb = _navmesh_aabb
 				navmesh_instance.navmesh.filter_baking_aabb_offset = -size / 2 + grid_location * _navmesh_aabb.size
-				navmesh_instance.connect("bake_finished", self, "_on_navmesh_bake_finished", [x, y, z])
-				_navesh_bake_begin_time[grid_location] = OS.get_ticks_msec()
-				navmesh_instance.bake_navigation_mesh()
 
-	while _navmeshes_to_rebake > 0:
-		yield(get_tree().create_timer(0.001), "timeout")
+	# Use rebake_all + yield instead of a direct call to _rebake_now() to let the editor run while baking
+	rebake_all()
+	yield(self, "_baking_process_finished")
 
-	var bake_end_time := OS.get_ticks_msec()
-	print("The complete navmesh took ", bake_end_time - bake_begin_time, "ms to bake")
+	var rebuild_end_time := OS.get_ticks_msec()
+	print("The complete rebuild took ", rebuild_end_time - rebuild_begin_time, "ms to complete")
+
+
+func rebake_all():
+	_baking_thread_mtx.lock()
+	_navmeshes_to_rebake = get_children()
+	_baking_thread_mtx.unlock()
+
+	_baking_request.post()
+
+
+func rebake_at(position: Vector3):
+	var local_position = to_local(position)
+	var to_process
+	for node in get_children():
+		var navmesh := node.navmesh as NavigationMesh
+		var real_aabb := navmesh.filter_baking_aabb
+		real_aabb.position += navmesh.filter_baking_aabb_offset
+		if real_aabb.has_point(local_position):
+			to_process = node
+			break
+
+	_baking_thread_mtx.lock()
+	_navmeshes_to_rebake = [to_process]
+	_baking_thread_mtx.unlock()
+
+	_baking_request.post()
 
 
 func clear():
@@ -95,11 +141,31 @@ func set_navigation(nav: NavigationMesh):
 	update_configuration_warning()
 
 
-func _on_navmesh_bake_finished(x: int, y: int, z: int):
-	var grid_location = Vector3(x, y, z)
-	_navesh_bake_duration[grid_location] = OS.get_ticks_msec() - _navesh_bake_begin_time[grid_location]
-	print("Navmesh at ", grid_location, " took ", _navesh_bake_duration[grid_location], "ms to bake")
-	_navmeshes_to_rebake -= 1
+func _rebaking_process():
+	while true:
+		_baking_request.wait()
+
+		_baking_thread_mtx.lock()
+		var should_exit = _exit_baking_thread
+		var to_process = _navmeshes_to_rebake
+		_baking_thread_mtx.unlock()
+
+		if should_exit:
+			break
+
+		_rebake_now(to_process)
+
+
+func _rebake_now(var to_process):
+	for node in to_process:
+		var navmesh_instance := node as NavigationMeshInstance
+		var navesh_bake_begin_time = OS.get_ticks_msec()
+		print("Start baking ", navmesh_instance)
+		navmesh_instance.bake_navigation_mesh(false)
+		var navesh_bake_end_time = OS.get_ticks_msec()
+		print("Navmesh took ", navesh_bake_end_time - navesh_bake_begin_time, "ms to bake")
+
+	emit_signal("_baking_process_finished")
 
 
 func _get_configuration_warning() -> String:
@@ -175,4 +241,4 @@ func property_get_revert(property: String):
 		return 1
 
 	if property == "edge_connection_margin":
-		return 1.0
+		return 3.0
